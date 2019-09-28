20f4ddd9991aa22e58dcbbc14601a6d47a38e531
Pavle Prica <pavle.prica5@gmail.com>
2019-09-28 14:09:30 +0200

    ASD

--- backend/src/main/java/com/globaltravel/globaltravel/controller/SmsController.java
+++ backend/src/main/java/com/globaltravel/globaltravel/controller/SmsController.java
@@ -13,9 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/sms")
 public class SmsController {
 
-    private static final String ACCOUNT_SID = "AC178684462ca3fa7ccab9e1a52e1395ab";
-    private static final String AUTH_TOKEN = "992cbd57aaa42f12b570f6bce1b1bab3";
-    private static final String PHONE_NUM = "+13343261433";
+    private static final String ACCOUNT_SID = "";
+    private static final String AUTH_TOKEN = "";
+    private static final String PHONE_NUM = "";
 
     @PostMapping
     public Object sendSms(@RequestParam String message, @RequestParam String phoneNumberToSend) {
